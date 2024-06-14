0e34a5e7ec1a78a4400fbab44415ceb627d82a92
Alexander Dimitrov <xavortm@gmail.com>
2024-06-14 17:45:26 +0300

    some comment additons

--- fundamentals/evaluation.ts
+++ fundamentals/evaluation.ts
@@ -7,12 +7,12 @@
  *
  * In our case, that would be a + b being substituted with the variable values to be
  * 1 + 2. This can further be evaluated as it's not a single value and we have the +
- * operand to run, resulting in "3". There are no operands and it's a single value,
+ * operand to run, resulting in "3". Now, there are no operands and it's a single value,
  * which we can't further evaluate/calculate, so we return it.
  *
- * The same way we evaluate a variable, we can evaluate a function (which can be
- * a variable on it's own as we know). So we need to run the function and result
- * in some kind of primitive value like number, string, array
+ * The same way we evaluate a variable, we can evaluate a function (to which a 
+ * variable can point). So we need to run the function and result in some kind
+ * of primitive value like number, string, array
  *
  * If a function is returned, that function will have to also be called in order
  * to be evaluated. Going down we reach to non-function primitive types. They are:
@@ -41,23 +41,29 @@ export default function evaluation(): number {
 
 // Inside evaluation:
 // Define variables:
-//    a <- 1
-//    b <- 2
+//    a ---> 1
+//    b ---> 2
 //
 // On the return, we run the sum line:
-//    return sum(a,b);
+//    return sum(a, b);
 //
 // Inside which, we evaluate a and b to 1 and 2:
-//    return sum(1,2);
+//    return sum(1, 2); (we pass the value to which the variables point)
+//
+// The function sum(a, b) is called with the values 1 and 2 as it's arguments:
+// a ---> 1;
+// b ---> 2;
 //
 // Inside sum(), we reach the return line:
 //    return a + b
 //
-// Where we evaluate a and b to 1 and 2:
+// In order to use the + operand, we need to evaluate a and b:
+// We evaluate a and b to 1 and 2:
 //    return 1 + 2
 //
 // We evaluate the expression to 3 and return it:
 //    return 3
 //
 // The returned value from sum(a,b) is evaluated to 3 and we return it.
+// The previous return sum(a, b) is evaluated to 3 and we return it as a result.
 //    return 3
